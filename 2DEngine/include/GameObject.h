e5ba782a2022d616aa359a7225a1b13432c96c4c
JuanOrjuelaA <72823028+JuanOrjuelaA@users.noreply.github.com>
2021-09-10 15:58:16 -0500

    Add game object abstract class

---
+++ 2DEngine/include/GameObject.h
@@ -0,0 +1,19 @@
+﻿#ifndef WITZ_ENGINE_GAME_OBJECT_H
+#define WITZ_ENGINE_GAME_OBJECT_H
+
+namespace WitzEngineObjects
+{
+	class GameObject
+	{
+	public:
+		virtual void draw() = 0;
+		virtual void update() = 0;
+		virtual void dispose() = 0;
+
+	protected:
+		GameObject() = default;
+		virtual ~GameObject() = default;
+	};
+}
+
+#endif
